46360fa69adebf978801cc0fb4b2a501a8fdac9f
Randy Thomas <randythomas@Randys-MBP.lan>
2022-03-17 18:50:21 -0700

    works

--- server/index.js
+++ server/index.js
@@ -6,8 +6,6 @@ const { VoiceResponse } = require('twilio').twiml; // TWILIO VOICE
 const db = require('../database/index');
 const mongoose = require("mongoose");
 
-
-
 //const port = 3000;
 const port = process.env.PORT || 3000;
 
