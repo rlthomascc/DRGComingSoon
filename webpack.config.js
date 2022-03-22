272dc864adca571cc5913f2aafb666e798481648
Randy Thomas <randythomas@Randys-MacBook-Pro.local>
2022-03-22 15:17:51 -0700

    test

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
       {
         test: /\.jsx?/,
         include: SRC_DIR,
-        loader: '@babel-loader',
+        loader: 'babel-loader',
         query: {
           presets: [
             '@babel/preset-env',
